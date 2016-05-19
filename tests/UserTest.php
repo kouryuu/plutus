244d6669e9f33458bbd141615767284642ae704d
kouryuu <antarticfreeze@gmail.com>
2016-05-19 14:20:38 -0500

    Added user testing.

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -4,19 +4,23 @@ class UserTest extends PHPUnit_Framework_TestCase
 {
   public function testCreateUser(){
     $user = new Plutus\User();
-    $user->createUser("testuser","Test User","simplePassword");
+    $user->createNewUser("testuser","Test User","simplePassword");
     $this->assertEquals("testuser",$user->getUserName());
+    $this->assertEquals("Test User",$user->getFullName());
+    $this->assertEquals("TEST USER",$user->getFullNameUpp());
 
   }
   public function testChangePassword(){
-    $user = new Plutus\User();
-    $user->createUser("testuser","Test User","simplePassword");
+    $user = new Plutus\User("testuser","Test User","simplePassword");
+    $user->createNewUser("testuser","Test User","simplePassword");
     $this->assertTrue($user->doPasswordsMatch("simplePassword"));
     $user->updatePasswordHash("anotherPassword");
     $this->assertFalse($user->doPasswordsMatch("simplePassword"));
     $this->assertFalse($user->doPasswordsMatch("anotherPassword "));
     $this->assertTrue($user->doPasswordsMatch("anotherPassword"));
   }
+
+
 }
 
 ?>
