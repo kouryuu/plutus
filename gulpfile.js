864f5f14a932a6c5ec19e16aabb10b7a5777811d
kouryuu <antarticfreeze@gmail.com>
2016-06-28 15:18:25 -0500

    Added build less and browserify tasks to gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
-var uglify = require('gulp-uglify')
+//var uglify = require('gulp-uglify')
 var watch = require('gulp-watch');
 var batch = require('gulp-batch');
 var less = require('gulp-less');
-var path = require('path');
+var browserify = require('browserify');
+var transform = require('vinyl-transform');
 
 gulp.task('build-less', function() {
   return gulp.src('web/assets/styles/less/*.less')
@@ -15,9 +16,19 @@ gulp.task('build-less', function() {
       }))
       .pipe(gulp.dest('web/assets/styles'));
 });
+gulp.task('bundle-js',function(){
+
+  return gulp.src('web/assets/js/*.js')
+  .pipe(transform(function(filename) {
+    return browserify(filename).bundle();
+    }))
+  .pipe(gulp.dest('web/assets/js/dist/'))
+});
+
+
 
 gulp.task('watch', function () {
     watch('web/assets/styles/less/*.less', batch(function (events, done) {
-        gulp.start('build', done);
+        gulp.start('build-less', done);
     }));
 });
