979691a28c516c2bbe76d4d47d4e86b63130d62a
kouryuu <antarticfreeze@gmail.com>
2016-06-28 20:01:48 -0500

    Separated Controller Logic to separate classes.

--- web/index.php
+++ web/index.php
@@ -2,12 +2,20 @@
 
 error_reporting(0); // Avoid weird header warnings.
 require_once __DIR__.'/../vendor/autoload.php';
+//
 require_once __DIR__.'/../lib/Account.php';
 require_once __DIR__.'/../lib/Transaction.php';
 require_once __DIR__.'/../lib/User.php';
 require_once __DIR__.'/../lib/Persistance.php';
+
+// Controllers
+require_once __DIR__.'/../lib/LoginCtrl.php';
+require_once __DIR__.'/../lib/UserCtrl.php';
+
+// Symfony namespaces
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
 $app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -23,64 +31,8 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
 $app->get('/',function() use ($app){
   return $app['twig']->render('index.twig');
 });
-
-
-$app->post('/new/user',function(Request $request) use ($app){
-  $user = new Plutus\User();
-  $response = array();
-  $response['errors'] = 0;
-  $user_name = $request->get('user_name');
-  $user_full_name = $request->get('user_full_name');
-  $user_password_hash = $request->get('user_password_hash');
-  if($user_name == null || $user_full_name == null || $user_password_hash == null ){
-    $response['errors'] += 1;
-    $response['error_desc'] = "Invalid request";
-    return json_encode($response);
-  }
-  $user_exists = $app['db']->fetchAll("SELECT * from User WHERE user_name='".$user_name."'");  //Should be escaped for XSS
-  if(sizeof($user_exists) == 0){
-    $user = new Plutus\User("","","");// I don't like this
-    $user->createNewUser($user_name,$user_full_name,$user_password_hash);
-    $user_pers = new Plutus\Persistance($user);
-    try{
-      $app['db']->executeQuery($user_pers->saveNewUserSQL());
-
-    }catch(Exception $e){
-      $response['errors'] += 1;
-      $response['error_desc'] = "Could not create new user";
-      //$e->getMessage();// This gets logged.
-    }
-
-  }else{
-    $response['errors'] += 1;
-    $response['error_desc'] = "User exists";
-  }
-  return json_encode($response);
-});
-$app->post('/login',function(Request $request) use ($app){
-  $response = array();
-  $response['errors'] = 0;
-  $user_name = $request->get('user_name');
-  $user_password_hash = $request->get('user_password_hash');
-  $user_exists = $app['db']->fetchAll("SELECT * from User WHERE user_name='".$user_name."'");  //Should be escaped for XSS
-    if(sizeof($user_exists) == 1){
-      $user_db = new Plutus\User($user_exists[0]['user_name'],$user_exists[0]['full_name'],$user_exists[0]['password_hash']);
-      if($user_db->doPasswordsMatch($user_password_hash)){
-        $response['user_name']= $user_exists[0]['user_name'];
-        $response['full_name']= $user_exists[0]['full_name'];
-        return json_encode($response);
-      }else{
-        // Passwords do not match
-        $response['errors'] += 1;
-        $response['error_desc'] = "Wrong password";
-      }
-    }else{
-      $response['errors'] += 1;
-      $response['error_desc'] = "Wrong user";
-    }
-    return json_encode($response);
-});
-
+$app->post('/new/user','Plutus\UserController::createUser');
+$app->post('/login','Plutus\LoginController::Login');
 
 
 $app->run();
