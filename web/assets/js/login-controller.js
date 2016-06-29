caf40687104fef90dba00b14ad8082b1a4382e4b
kouryuu <antarticfreeze@gmail.com>
2016-06-28 20:03:57 -0500

    Modified post sending method to avoid sending in JSON.

--- web/assets/js/login-controller.js
+++ web/assets/js/login-controller.js
@@ -7,16 +7,19 @@ loginApp.controller('TestController',['$scope','$http','$log',function($scope,$h
   $scope.test = (function(){$log.log($scope.username)});
   $scope.logmein = (function(){
     $http({
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
       method: 'POST',
       url: '/login',
-      data: {
+      data: $.param({ // This is needed due to AngularJS default JSON post
         'user_name':$scope.username,
         'user_password_hash':$scope.password
-      }
+      })
     }).then(function successCallback(response){
       // Success
       $log.log(response);
+      if(response.data.errors > 0){
       $scope.login_errors = response.data.error_desc;
+      }
     },function errorCallback(response){
     });
   });
